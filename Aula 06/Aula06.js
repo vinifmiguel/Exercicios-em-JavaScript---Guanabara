98ddc3434bf1d9e3d06639c1427e4eb4279568f5
Vinicius Freitas Miguel <vinifmiguel2@gmail.com>
2026-02-18 15:40:08 -0300

    Add alguns comandos da Aula 06
    Adicionando alguns comandos básicos vistos na aula 06!

---
+++ Aula 06/Aula06.js
@@ -0,0 +1,5 @@
+let n1 = Number.parseFloat(prompt("Qual o primeiro número?"));
+let n2 = Number.parseFloat(prompt("Digite segundo número: "));
+let resultado = n1+n2;
+
+alert(`A soma entre ${n1} e ${n2} é: ${resultado}`);
